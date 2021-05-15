66cabfa8d24f1573b96c33bc6e6e7438a53c8f00
David Polania <davidpolaniaac@users.noreply.github.com>
2021-05-15 15:57:37 -0500

    add observe

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+
+const observe : Observer<any> = {
+    next: value => console.log("next", value),
+    error: error => console.warn('error', error),
+    complete: () => console.log('Complete')
+
+    
+};
 
 const obs$ = new Observable<string>(subs => {
 
@@ -7,6 +16,10 @@ const obs$ = new Observable<string>(subs => {
     subs.complete();
 });
 
+obs$.subscribe(observe);
+
+obs$.subscribe(subs => console.log(subs));
+obs$.subscribe(console.log);
 obs$.subscribe(
     value => console.log('value', value),
     error => console.warn('error', error),
